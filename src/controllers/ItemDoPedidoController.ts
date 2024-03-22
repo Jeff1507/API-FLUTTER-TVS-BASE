215f2cfb55f5402191f5e7b64a93ea9821a77146
jefferson <jeffchaves1507@gmail.com>
2024-03-22 14:12:25 -0300

    aaa

--- src/controllers/ItemDoPedidoController.ts
+++ src/controllers/ItemDoPedidoController.ts
@@ -1,6 +1,20 @@
 import { Request, Response } from 'express';
 import { Produto } from '../models/Produto';
 import { Pedido } from '../models/Pedido';
-import { ItemDoPedido } from '../models/ItemDoPedido';
+import { ItemDoPedido, ItemDoPedido } from '../models/ItemDoPedido';
 
+export const getItemDoPedidoById = async (req: Request, res: Response) => {
+    try {
+        const itemDoPedidoId = parseInt(req.params.id, 10);
+        const itemDoPedido = await ItemDoPedido.findByPk(itemDoPedidoId);
 
+        if (itemDoPedido) {
+            res.json(itemDoPedido);
+        } else {
+            res.status(404).json({message: 'Item do Pedido não encontrado'});
+        }
+    } catch (error) {
+        console.error('Erro ao buscar cliente:', error);
+        res.status(500).json({message: 'Erro ao buscar cliente'});
+    }
+};
