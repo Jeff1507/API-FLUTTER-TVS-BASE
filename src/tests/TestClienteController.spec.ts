db083b38256b7af89be104178f6d4b5e4d5a170f
jefferson <jeffchaves1507@gmail.com>
2024-03-22 13:52:45 -0300

    arruma o env

--- src/tests/TestClienteController.spec.ts
+++ src/tests/TestClienteController.spec.ts
@@ -2,6 +2,7 @@ const request = require('supertest');
 import * as server from '../server';
 const app = server.server;
 import { Request, Response } from 'express';
+import { ItemDoPedido } from '../models/ItemDoPedido';
 
 describe('Teste da rota getClienteById', () => {
     it('deve retornar o cliente correto quando o ID é valido', async () => {
@@ -11,4 +12,13 @@ describe('Teste da rota getClienteById', () => {
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('id', clienteId);
     });
+    it('deve retornar um status 404 quando o ID do cliente não existe', async () => {
+        const clienteId = 9999;
+
+        const response = await request(app).get(`/clientes/${clienteId}`);
+
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty('message', 'cliente não encontrado');
+    });
 })
+
